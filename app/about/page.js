2ce951a30322878de7633054b7a6190dd718304b
JeffLi117 <li.jeffreyt@yahoo.com>
2023-05-14 19:24:50 -0500

    Started updating about section

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+
 
 function aboutPage() {
   return (
-    <div>aboutPage</div>
+    <div>
+        <div className="flex justify-left items-center">
+            <div className="text-2xl">About</div>&nbsp;
+            <hr className="border-1 border-solid w-28" />
+        </div>
+        <div className="p-4">
+            <div className="grid" style={{ gridTemplateColumns: '40% 60%' }}>
+                <Image src="/headshot.jpg" alt="professional headshot of Jeffrey Li" width="0" height="0" sizes="100vw" style={{ width: '60%', height: 'auto' }} className="rounded-lg"/>
+                <div>Full-Stack Software Engineer</div>
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
