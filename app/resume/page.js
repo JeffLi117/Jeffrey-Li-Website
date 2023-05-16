3a6136cda6ebd190739c366052088dae878ccb77
JeffLi117 <li.jeffreyt@yahoo.com>
2023-05-16 16:04:27 -0500

    Finished initial version of resume page

--- app/resume/page.js
+++ app/resume/page.js
@@ -9,29 +9,55 @@ function resumePage() {
         <div className="grid grid-cols-1 justify-left items-start pt-4 md:grid-cols-2">
             <div className="md:col-start-1">
               <div className="text-2xl mb-4 font-bold w-4/5 md:text-3xl">Professional Experience</div>
+              <li className="text-1xl bulleted-header m-2 font-bold w-4/5 md:text-2xl"><span>Front-End Developer</span></li>
+              <div className="m-3 mb-2 font-semibold">Chingu (Apr 2023 - Present)</div>
+              <li className="mx-3 mb-1"><span>Regularly pair program with team members to improve team productivity and coding skills</span></li>
+              <li className="mx-3 mb-1"><span>Participated in Agile sprint meetings resulting in successful project delivery</span></li>
+              <li className="mx-3 mb-1"><span>Contributed to organizing file structure for application and built out higher-level JavaScript/React components
+improving code readability and maintainability</span></li>
+              
+              <li className="text-1xl bulleted-header m-2 font-bold w-4/5 md:text-2xl"><span>Freelance Front-End Developer</span></li>
+              <div className="m-3 mb-2 font-semibold">Self-Employed (March 2023 - Present)</div>
+              <li className="mx-3 mb-1"><span>Used entrepreneurial expertise, technical skills, and creative talents to develop, market, and maintain websites</span></li>
+              <li className="mx-3 mb-1"><span>Worked independently across several projects to meet deadlines</span></li>
+              <li className="mx-3 mb-1"><span>Skilled in React, JavaScript, APIs, and CSS to create responsive and data-intensive web designs</span></li>
+
+              <li className="text-1xl bulleted-header m-2 font-bold w-4/5 md:text-2xl"><span>Resident Physician</span></li>
+              <div className="m-3 mb-2 font-semibold">HCA Healthcare (June 2022 - Oct 2022)</div>
+              <li className="mx-3 mb-1"><span>Led multi-disciplinary teams to design, develop, and implement care plans for patients with best practices
+as a Certified Professional in Patient Safety</span></li>
+              <li className="mx-3 mb-1"><span>Planned & proactively coordinated inpatient care plans based on timely critical evaluation, troubleshooting,
+& interdisciplinary communication</span></li>
+            
             </div>
             <div className="md:col-start-2">
             <div className="text-2xl mb-4 font-bold w-4/5 md:text-3xl">Education</div>
-              <div className="text-1xl m-2 font-bold w-4/5 md:text-2xl">Full-Stack Software Developer</div>
+              <li className="text-1xl bulleted-header m-2 font-bold w-4/5 md:text-2xl"><span>Full-Stack Software Developer</span></li>
               <div className="m-3 mb-2 font-semibold">The Odin Project</div>
               <div className="m-3">An immersive course on full-stack software development. By following their comprehensive full-stack curriculum, I gained proficiency in front-end and back-end technologies, along with a solid foundation in computer science principles.</div>
-              <div className="text-1xl m-2 font-bold w-4/5 md:text-2xl">Doctor Of Osteopathic Medicine</div>
+
+              <li className="text-1xl bulleted-header m-2 font-bold w-4/5 md:text-2xl"><span>Doctor Of Osteopathic Medicine</span></li>
               <div className="m-3 mb-2 font-semibold">Texas College Of Osteopathic Medicine</div>
-              <div className="text-1xl m-2 font-bold w-4/5 md:text-2xl">Master Of Medical Sciences</div>
+
+              <li className="text-1xl bulleted-header m-2 font-bold w-4/5 md:text-2xl"><span>Master Of Medical Sciences</span></li>
               <div className="m-3 mb-2 font-semibold">University of North Texas Health Science Center</div>
               <div className="text-2xl mb-4 font-bold w-4/5 md:text-3xl">Stack</div>
-              <div className="m-3 flex flex-row justify-left items-center" >
-                <div className="stack-image-holder">
+              <div className="m-3 flex flex-col justify-left items-start" >
+                <div className="flex flex-row gap-4 stack-image-holder">
                   <Image src="/js.png" alt="javascript icon" width="0" height="0" sizes="100vw" className="w-full h-auto" />
+                  <div className="text-xl">JavaScript</div>
                 </div>
-                <div className="stack-image-holder">
-                  <Image src="/react.png" alt="react icon" width="0" height="0" sizes="100vw" className="w-full h-auto" />
+                <div className="flex flex-row gap-4 stack-image-holder">
+                  <Image src="/react.png" alt="react icon" width="0" height="0" sizes="100vw" className="w-full h-auto m-auto" />
+                  <div className="text-xl">React</div>
                 </div>
-                <div className="stack-image-holder">
+                <div className="flex flex-row gap-4 stack-image-holder">
                   <Image src="/firebase.png" alt="firebase icon" width="0" height="0" sizes="100vw" className="w-full h-auto" />
+                  <div className="text-xl">Firebase</div>
                 </div>
-                <div className="stack-image-holder">
+                <div className="flex flex-row gap-4 stack-image-holder">
                   <Image src="/sveltekit.png" alt="sveltekit icon" width="0" height="0" sizes="100vw" className="h-full w-auto m-auto" />
+                  <div className="text-xl">SvelteKit</div>
                 </div>
               </div>
             </div>
