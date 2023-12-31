e7aecfefc5d91bf82f24cff3926fbd13e876f785
JeffLi117 <li.jeffreyt@yahoo.com>
2023-12-31 13:22:38 -0600

    Modify footer margins and padding

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
 
   return (
-    <section className="absolute w-1/2 md:w-1/4 bottom-1 pb-1">
-      <hr className="border-b-4 border-solid rounded-full border-purple-100 mb-2 mx-2"/>
+    <section className="absolute w-full lg:w-[75%] bottom-1 pb-1">
+      <hr className="border-b-4 w-1/2 md:w-1/4 border-solid rounded-full border-purple-100 mb-2 mx-2"/>
       <div className="flex justify-between items-center mx-2">
-        <span>©️ 2023 Jeffrey Li</span>
+        <span className="w-full">©️ 2023 Jeffrey Li</span>
       </div>
     </section>
   )
