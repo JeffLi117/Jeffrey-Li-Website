40d99c29d19c6161f18b2c8b5a827f73e95b6086
JeffLi117 <li.jeffreyt@yahoo.com>
2023-12-31 16:11:39 -0600

    Add back buttons to all non-landing page routes

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,9 +1,11 @@
 import EmailMeForm from "@/components/EmailMeForm";
+import BackButton from "@/components/BackButton";
 
 function contactPage() {
 
     return (
         <section className="relative h-full pb-12 md:pb-8">
+            <BackButton destination="" />
             <div className={`flex flex-col justify-start items-start gap-4 p-2`}>
                 <div className="text-3xl text-bold mb-1">Contact Me</div>
                 <div className="mb-4">If you'd like to chat about a project, need help building something, or just have a question, please fill out the form below. I'll get back to you as soon as I can!</div>
